11fd434540a4ddf79ddda0ddd254417a0cb36198
matthcol <matthias@learning64.net>
2021-10-13 15:30:03 +0200

    improve app

--- src/Application.cs
+++ src/Application.cs
@@ -2,6 +2,8 @@
 public class Application {
 
 	public void Main() {
+		var movie = new Movie {Title = "Dune Pilat"};
 		Console.WriteLine("My app is good");
+		Console.WriteLine($"A good movie: {movie.Title}");
 	}
 }
