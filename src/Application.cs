7cf27ada9f99f6488ea3c0f0f0423148cfbeaee7
Tristan Kahn <tkahn@report-one.com>
2021-10-13 17:28:13 +0200

    add oui oui

--- src/Application.cs
+++ src/Application.cs
@@ -4,6 +4,7 @@ public class Application {
 	public void Main() {
 		var movie = new Movie { Title = "Dune" };
 		var movie2 = new Movie { Title = "Mourir peut attendre" };
+		var movie2 = new Movie { Title = "Oui Oui" };
 		var star = new Movie { Name = "Timothée Chalamet" };
 		
 		Console.WriteLine("My app is good");
