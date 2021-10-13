f63a5f03242b4b93d0bdbb7798bb8106360a2049
matthcol <matthias@learning64.net>
2021-10-13 15:40:07 +0200

    another version of app

--- src/Application.cs
+++ src/Application.cs
@@ -2,8 +2,6 @@
 public class Application {
 
 	public void Main() {
-		var movie = new Movie {Title = "Dune Pilat"};
 		Console.WriteLine("My app is good");
-		Console.WriteLine($"A good movie: {movie.Title}");
 	}
 }
