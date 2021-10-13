ce79c1748dbc9dccb9e58ecd6811efb579155084
matthcol <matthias@learning64.net>
2021-10-13 17:15:57 +0200

    add 007 movie

--- src/Application.cs
+++ src/Application.cs
@@ -3,6 +3,7 @@ public class Application {
 
 	public void Main() {
 		var movie = new Movie { Title = "Dune" };
+		var movie2 = new Movie { Title = "Mourir peut attendre" };
 		var star = new Movie { Name = "Timothée Chalamet" };
 		
 		Console.WriteLine("My app is good");
