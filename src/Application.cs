79cfc3416c126e6595aea080f1f77e512cd8867d
matthcol <matthias.colin@gmail.com>
2021-10-13 17:34:35 +0200

    add Fast n Fusrious

--- src/Application.cs
+++ src/Application.cs
@@ -2,8 +2,9 @@
 public class Application {
 
 	public void Main() {
-		var movie = new Movie { Title = "Dune" };
+		var movie = new Movie { Title = "Dune 2021" };
 		var movie2 = new Movie { Title = "Mourir peut attendre" };
+		var movie3 = new Movie { Title = "Fast and Furious 9" };
 		var star = new Movie { Name = "Timothée Chalamet" };
 		
 		Console.WriteLine("My app is good");
