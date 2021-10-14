1b742daf38040f1a834bd86e21e6a6919b4adf5a
Tristan Kahn <tkahn@report-one.com>
2021-10-14 09:35:12 +0200

    Fix Compilation 2

--- src/Movie.cs
+++ src/Movie.cs
@@ -1,10 +1,13 @@
-public class Movie {
-	
-	public String Title { get; set; }
-	
-	public int Year { get; set; }
-	
-	public int Duration { get; set; }
-	
-	public string Synopsis { get; set; }
+namespace MyApp
+{
+	public class Movie {
+		
+		public String Title { get; set; }
+		
+		public int Year { get; set; }
+		
+		public int Duration { get; set; }
+		
+		public string Synopsis { get; set; }
+	}
 }
