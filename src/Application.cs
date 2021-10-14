98833be1585ddd8fa9a208026915324f97f2f059
Tristan Kahn <tkahn@report-one.com>
2021-10-14 09:24:06 +0200

    Fix Compilation

--- src/Application.cs
+++ src/Application.cs
@@ -1,15 +1,22 @@
+using System;
 
-public class Application {
+namespace MyApp
+{
+	public class Application {
 
-	public void Main() {
-		var movie0 = new Movie { Title = "Dune", Year = 2021 };
-		var movie1 = new Movie { Title = "Dune", Year = 1984 };
-		var movie2 = new Movie { Title = "Mourir peut attendre" };
-		var movie3 = new Movie { Title = "Fast and Furious 9" };
-		var movie4 = new Movie { Title = "Oui Oui" };
-		var movie5 = new Movie { Title = "Sqwid Game" };
-		var star = new Movie { Name = "Timothée Chalamet" };
-		
-		Console.WriteLine("My app is good");
+		static int Main(string[] args)
+		{
+			var movie0 = new Movie { Title = "Dune", Year = 2021 };
+			var movie1 = new Movie { Title = "Dune", Year = 1984 };
+			var movie2 = new Movie { Title = "Mourir peut attendre" };
+			var movie3 = new Movie { Title = "Fast and Furious 9" };
+			var movie4 = new Movie { Title = "Oui Oui" };
+			var movie5 = new Movie { Title = "Sqwid Game" };
+			var star = new Movie { Name = "Timothée Chalamet" };
+			
+			Console.WriteLine("My app is good");
+
+			return 0;
+		}
 	}
 }
